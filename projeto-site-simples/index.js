dd0f220bfcdc2c654b1e1746ca02537d0e25d204
andersonr-o <97858145+andersonr-o@users.noreply.github.com>
2022-03-07 22:23:07 -0300

    Update index.js

--- projeto-site-simples/index.js
+++ projeto-site-simples/index.js
@@ -1,11 +1,17 @@
 var thanks = window.document.getElementById('h1dif')
 var cor = window.document.getElementById('votou')
+var box = window.document.getElementById('votos')
 /*Ou usamos -> cor.addEventListener('click', corsim)*/
 
 function corsim(){
     cor.style.background = 'aqua'
     thanks.innerHTML = 'OBRIGADO POR VOTAR!'
+    if(Number(box.value) >= 5 || Number(box.value) <= 0){
+        window.alert(`Insira um número válido`)
+    }else{
+        alert(`ok`)
     }
+ }
 
 function n1(){
     thanks.style.color  = 'darkblue'
